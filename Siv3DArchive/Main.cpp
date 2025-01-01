49d2153105bef5b6892a9631b6f7d7787e883c9d
Shiba256 <153472750+Tomoyuki-Kawaguchi@users.noreply.github.com>
2025-01-01 13:56:57 +0900

    Update ReadMe

--- Siv3DArchive/Main.cpp
+++ Siv3DArchive/Main.cpp
@@ -10,7 +10,7 @@ void Main() {
 	const AES::Key key = AES::Key::FromBase64Str(key_str);
 
 	// アーカイブの入出力先と鍵を設定
-	Archive::Setting(U"archive.cmp", key, AES::Mode::CFB);
+	Archive::Setting(U"archive.cmp", key, AES::Mode::ECB);
 
 	// engine ディレクトリのアーカイブを作成
 	Archive::Create(U"engine");
